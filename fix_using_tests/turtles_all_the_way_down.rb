286400e4f3f2093e0646e315b6d5ad73389bdb68
Dominic Chu <dominic.m.chu@gmail.com>
2019-10-17 17:11:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -9,9 +9,7 @@ def turtles
 end
 
 def turtle_traits(turtles)
-  turtles.map do |turtle|
-    turtle[:traits].each do |trait|
-      trait
-    end
+  turtles.each do |turtle|
+    turtle[:traits]
   end
 end
