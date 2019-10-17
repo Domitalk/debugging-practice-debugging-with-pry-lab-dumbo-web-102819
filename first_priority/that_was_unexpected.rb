8b1404c92e39a9861cf5a1d78dba476ef2e174d3
Dominic Chu <dominic.m.chu@gmail.com>
2019-10-17 16:38:31 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,10 +1,5 @@
-require_relative '../first_priority/you_called'
-require 'pry'
-
 class Person
-
   attr_accessor :name, :candy_lover
-  binding.pry
   def initialize(name, candy_lover)
     @name = name
     @candy_lover = candy_lover
@@ -29,7 +24,5 @@ class Person
       10.times do
         "not a ballin candy lover"
     end
-
   end
-
 end
